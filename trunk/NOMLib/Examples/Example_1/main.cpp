6d07a502a45ca7ec7d550ce84dfcfcaa72b767c5
tottek@gmail.com <tottek@gmail.com@6ce1f5ab-bddf-3397-67f4-0d3c53ff36db>
2012-01-04 01:24:07 +0000

    Very small change..

--- trunk/NOMLib/Examples/Example_1/main.cpp
+++ trunk/NOMLib/Examples/Example_1/main.cpp
@@ -1,19 +1,21 @@
-#include <string>
+#include <conio.h>
 #include <iostream>
 #include "NOMLib.h"
 
 using namespace std;
 int main()
 {
-	cout<<"Hello NOMLib. Testing a function from the library\n";
+	cout<<"Hello NOMLib. \nTesting a function from the library\n";
 	
+
 	int returnVal = loadSBML("NONSENSE");
 
 	cout<<"The return value was: "<<returnVal<<endl;
 	cout<<"Last error was: "<<getError()<<endl;
+	
 	cout<<"Hit any key to exit...";
-
-	char ch;
-	cin>>ch;
+	cin.ignore(0,'\n');
+    getch();
+	
     return 0;
 }
