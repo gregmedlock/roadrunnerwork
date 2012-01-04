67a60006e9815909501c62505f87aed27df14424
tottek@gmail.com <tottek@gmail.com@6ce1f5ab-bddf-3397-67f4-0d3c53ff36db>
2012-01-04 02:19:20 +0000

    Preparing to compile roadrunner..

--- trunk/NOMLib/Examples/Example_1/main.cpp
+++ trunk/NOMLib/Examples/Example_1/main.cpp
@@ -6,13 +6,14 @@ using namespace std;
 int main()
 {
 	cout<<"Hello NOMLib. \nTesting a function from the library\n";
-	
 
 	int returnVal = loadSBML("NONSENSE");
 
 	cout<<"The return value was: "<<returnVal<<endl;
 	cout<<"Last error was: "<<getError()<<endl;
-	
+
+
+	//-------------------------------------	
 	cout<<"Hit any key to exit...";
 	cin.ignore(0,'\n');
     getch();
