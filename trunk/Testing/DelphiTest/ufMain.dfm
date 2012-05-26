b69135ff374e55df3dff05731878ebb24ed77865
hsauro <hsauro@6ce1f5ab-bddf-3397-67f4-0d3c53ff36db>
2012-05-26 16:45:45 +0000

    Added testing code and dependency files, eg uMatrix.pas

--- trunk/Testing/DelphiTest/ufMain.dfm
+++ trunk/Testing/DelphiTest/ufMain.dfm
@@ -3,7 +3,7 @@ object Form2: TForm2
   Top = 0
   Caption = 'Test uRoadRunnerAPI.pas'
   ClientHeight = 339
-  ClientWidth = 481
+  ClientWidth = 566
   Color = clBtnFace
   Font.Charset = DEFAULT_CHARSET
   Font.Color = clWindowText
@@ -26,10 +26,11 @@ object Form2: TForm2
   object pnlBottm: TPanel
     Left = 0
     Top = 298
-    Width = 481
+    Width = 566
     Height = 41
     Align = alBottom
     TabOrder = 1
+    ExplicitWidth = 481
     object lblProgress: TLabel
       Left = 8
       Top = 16
@@ -46,5 +47,25 @@ object Form2: TForm2
     Caption = 'Get Copyright'
     TabOrder = 2
     OnClick = btnGetCopyrightClick
+  end
+  object btnLoadSBML: TButton
+    Left = 24
+    Top = 78
+    Width = 89
+    Height = 25
+    Caption = 'Load SBML'
+    TabOrder = 3
+    OnClick = btnLoadSBMLClick
+  end
+  object grid: TStringGrid
+    Left = 144
+    Top = 0
+    Width = 422
+    Height = 298
+    Align = alRight
+    Anchors = [akLeft, akTop, akBottom]
+    DefaultColWidth = 84
+    FixedCols = 0
+    TabOrder = 4
   end
 end
--- trunk/Testing/DelphiTest/ufMain.pas
+++ trunk/Testing/DelphiTest/ufMain.pas
@@ -4,7 +4,7 @@ interface
 
 uses
   Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
-  Dialogs, StdCtrls, uRoadRunnerAPI, ExtCtrls;
+  Dialogs, StdCtrls, uRoadRunnerAPI, ExtCtrls, IOUtils, Grids;
 
 type
   TForm2 = class(TForm)
@@ -12,8 +12,11 @@ type
     pnlBottm: TPanel;
     lblProgress: TLabel;
     btnGetCopyright: TButton;
+    btnLoadSBML: TButton;
+    grid: TStringGrid;
     procedure Button1Click(Sender: TObject);
     procedure btnGetCopyrightClick(Sender: TObject);
+    procedure btnLoadSBMLClick(Sender: TObject);
   private
     { Private declarations }
   public
@@ -27,10 +30,29 @@ implementation
 
 {$R *.dfm}
 
+Uses uMatrix;
+
 procedure TForm2.btnGetCopyrightClick(Sender: TObject);
 begin
   lblProgress.caption := getCopyright;
 end;
+
+procedure TForm2.btnLoadSBMLClick(Sender: TObject);
+var str : AnsiString;
+    m : TMatrix;
+    i, j : integer;
+begin
+  str := TFile.ReadAllText('feedback.xml');
+  if not loadSBML(str) then
+     lblProgress.Caption := 'Failed to load SBML model';
+  m := simulate();
+  grid.ColCount := m.c + 1;
+  grid.RowCount := m.r + 1;
+  for i := 1 to m.r do
+      for j := 1 to m.c do
+          grid.Cells [j-1, i] := Format ('%8.5g', [m[i,j]]);
+end;
+
 
 procedure TForm2.Button1Click(Sender: TObject);
 var errMsg : AnsiString;
