3d7632564235c1246011c5f71764f30249b26e42
tottek <tottek@6ce1f5ab-bddf-3397-67f4-0d3c53ff36db>
2012-05-29 02:25:17 +0000

    Only Debug target works for now rr_c is not compiling with Visual Studio.. linker error to GetOptions, which actually makes sense. Need to be a c-api function.

--- trunk/src/rrGetOptions.h
+++ trunk/src/rrGetOptions.h
@@ -4,10 +4,10 @@
 //---------------------------------------------------------------------------
 namespace rr
 {
-RR_DECLSPEC     extern int                         optind;
-RR_DECLSPEC     extern int                         opterr;
-RR_DECLSPEC     extern char                       *optarg;
-RR_DECLSPEC int   GetOptions(int argc, char *argv[], char *optstring);
+RR_DECLSPEC     extern  int                         optind;
+RR_DECLSPEC     extern  int                         opterr;
+RR_DECLSPEC     extern  char                       *optarg;
+RR_DECLSPEC             int   GetOptions(int argc, char *argv[], char *optstring);
 }
 
 #endif
