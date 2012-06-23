5be405b7d523e2ce74ffdab1b7b77818cca6a711
hsauro <hsauro@6ce1f5ab-bddf-3397-67f4-0d3c53ff36db>
2012-06-23 01:15:01 +0000

    Added header comments and license to rr_c_api.cpp and rr_c_types.h

--- source/trunk/Wrappers/C/rr_c_api.h
+++ source/trunk/Wrappers/C/rr_c_api.h
@@ -6,10 +6,9 @@
  * @brief roadRunner C API 2012
  * @author Totte Karlsson & Herbert M Sauro
  * 
- * <!--------------------------------------------------------
+ * <--------------------------------------------------------------
  * This file is part of cRoadRunner. 
- * See http://code.google.com/p/roadrunnerwork/
- * for more details.
+ * See http://code.google.com/p/roadrunnerwork/ for more details.
  * 
  * Copyright (C) 2012
  *   University of Washington, Seattle, WA, USA
@@ -20,27 +19,27 @@
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
- *  In plain english this means:
+ * In plain english this means:
  *
  * You CAN freely download and use this software, in whole or in part, for personal,
  * company internal, or commercial purposes;
  *
  * You CAN use the software in packages or distributions that you create.
  *
- *  You SHOULD include a copy of the license in any redistribution you may make;
+ * You SHOULD include a copy of the license in any redistribution you may make;
  *
  * You are NOT required include the source of software, or of any modifications you may
  * have made to it, in any redistribution you may assemble that includes it.
  *
  * YOU CANNOT:
  *
- *  redistribute any piece of this software without proper attribution;
+ * redistribute any piece of this software without proper attribution;
 */
 
 #if defined(__cplusplus)
