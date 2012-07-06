540b838188cb50e658e3e8f55c0c01b35d0649c1
tottek <tottek@6ce1f5ab-bddf-3397-67f4-0d3c53ff36db>
2012-07-06 20:00:00 +0000

    Work on ArrayList

---
+++ source/trunk/Testing/unit_tests/ArrayList/main.cpp
@@ -0,0 +1,32 @@
+//---------------------------------------------------------------------------
+
+#pragma hdrstop
+#include "rrArrayList.h"
+//---------------------------------------------------------------------------
+#pragma argsused
+
+using namespace rr;
+int main()
+{
+    ArrayList<int> list1;
+    ArrayList<string> list2;
+    ArrayList<string> list3;
+
+//    list1.Add("item1");
+//    list2.Add("1a");
+//    list2.Add("1b");
+//    list2.Add("1c");
+//    list1.Add(list2);
+
+//    list2.Clear();
+//
+//    list1.Add("item2");
+//    list2.Add("2a");
+//    list2.Add("2b");
+//    list2.Add("2c");
+//    list1.Add(list2);
+//
+//    list3.Add(list1);
+    return 0;
+}
+
--- source/trunk/src/rrArrayList.h
+++ source/trunk/src/rrArrayList.h
@@ -2,6 +2,7 @@
 #define rrArrayListH
 #include <vector>
 #include <string>
+#include <list>
 #include "rrObject.h"
 #include "rrStringList.h"
 using std::list;
@@ -11,38 +12,187 @@ using std::string;
 namespace rr
 {
 
-class RR_DECLSPEC ArrayStringList : public rrObject
+template <class T>
+class ArrayListItem
 {
+    public:
+        ArrayListItem          *mPrevious;
+        list<ArrayListItem<T>* >     value;
+        ArrayListItem          *mNext;
+};
+
+template <class T>
+class ArrayList : public rrObject
+{
+    //This list can only hold items of type T
     protected:
-        string                          mLabel;
-        vector< list<string> >          mContainer;
+        ArrayListItem<T>               *mPrevious;
+        list< ArrayListItem<T>* >      *mNext;
+        ArrayListItem<T>               *mCurrent;
 
     public:
                                         ArrayList();
-                                        ArrayList(const string& lbl, const ArrayList& cp);
-                                        ArrayList(const ArrayList& cp);
-                                        ArrayList(const StringList& cp);
-        void                            operator=(const ArrayList& rhs);
-        void                            Add(const ArrayList& lists);
-        void                            Add(const string& lbl, const ArrayList& lists);
-        void                            Clear();
+//                                        ArrayList(const ArrayList& cp);
+//                                        ArrayList(const StringList& cp);
+//        void                            operator=(const ArrayList& rhs);
+//        void                            Add(const ArrayList<T>& list);
+//        void                            Clear();
 
-        void                            Add(const string& listName, const StringList& coll);
-        void                            Add(const StringList& coll);
-        void                            Add(const string& coll);
-        void                            Add(const int& coll);
-        int                             Count() const;
-        int                             ListCount() const;
-        int                             TotalCount() const;
-        StringList&                     operator[](const int& index);
-        const StringList&               operator[](const int& index) const;
-        vector<StringList>::iterator    begin();
-        vector<StringList>::iterator    end();
+//        void                            Add(const T& item);
+//        void                            Add(const string& listName, const StringList& coll);
+//        void                            Add(const StringList& coll);
+
+
+//        int                             Count() const;
+//        int                             ListCount() const;
+//        int                             TotalCount() const;
+//        StringList&                     operator[](const int& index);
+//        const StringList&               operator[](const int& index) const;
+//        vector<StringList>::iterator    begin();
+//        vector<StringList>::iterator    end();
 };
 
-ostream& operator<<(ostream& stream, const ArrayList& list);
+//ostream& operator<<(ostream& stream, const ArrayList& list);
 
-//typedef ArrayList ArrayList; //ArrayList is C# name..
-//typedef ArrayList StringCollection;
+template<class T>
+ArrayList<T>::ArrayList()
+{
+}
+
+//template<class T>
+//ArrayList<T>::ArrayList::ArrayList(const ArrayList& cp)
+//:
+//mContainer(cp.mContainer)
+//{}
+//
+//template<class T>
+//ArrayList<T>::ArrayList::ArrayList(const string& lbl, const ArrayList& cp)
+//:
+//mLabel(lbl),
+//mContainer(cp.mContainer)
+//{}
+//
+//template<class T>
+//void ArrayList<T>::ArrayList::Clear()
+//{
+//    mContainer.empty();
+//}
+
+//template<class T>
+//ArrayList<T>::ArrayList(const StringList& cp)
+//{
+//    Add(cp.mLabel, cp);
+//}
+
+//void ArrayList::operator=(const ArrayList& rhs)
+//{
+//
+//}
+
+//template<class T>
+//int ArrayList::TotalCount() const
+//{
+//    //Returns the total count of all list items..
+//    int cnt = 0;
+//    for(int i = 0; i < Count(); i++)
+//    {
+//        cnt += mContainer[i].Count();
+//    }
+//    return cnt;
+//}
+
+//template<class T>
+//int ArrayList::ListCount() const
+//{
+//    return mContainer.size();
+//}
+//
+//template<class T>
+//int ArrayList::Count() const
+//{
+//    return mContainer.size();
+//}
+
+//template<class T>
+//StringList& ArrayList::operator[](const int& index)
+//{
+//    return mContainer[index];
+//}
+
+//template<class T>
+//const StringList& ArrayList::operator[](const int& index) const
+//{
+//    return mContainer[index];
+//}
+//
+//template<class T>
+//vector<StringList>::iterator ArrayList::begin()
+//{
+//    return mContainer.begin();
+//}
+//
+//template<class T>
+//vector<StringList>::iterator ArrayList<T>::end()
+//{
+//    return mContainer.end();
+//}
+
+//template<class T>
+//void ArrayList<T>::Add(const ArrayList<T>& lists)
+//{
+//    Add(lists);
+//}
+//
+//template<class T>
+//void ArrayList<T>::Add(const ArrayList<T>& lists)
+//{
+//    for(int i = 0; i < lists.Count(); i++)
+//    {
+//        StringList aList;
+//        aList = lists.mContainer[i];    //Todo: lists[i] should work...
+//        Add(aList);
+//    }
+//}
+//
+//template<class T>
+//void ArrayList<T>::Add(const StringList& list)
+//{
+//    mContainer.push_back(list);
+//}
+
+//template<class T>
+//void ArrayList<T>::Add(const string& listName, const StringList& aList)
+//{
+//    StringList list(aList);
+//    list.Label(listName);
+//    mContainer.push_back(list);
+//}
+//
+//template<class T>
+//void ArrayList<T>::Add(const string& item)
+//{
+//    StringList list;
+//    list.push_back(item);
+//    Add(list);
+//
+//}
+//
+//template<class T>
+//void ArrayList<T>::Add(const int& atPos)
+//{
+//
+//}
+
+//ostream& operator<<(ostream& stream, const ArrayList& list)
+//{
+//    vector<StringList>::iterator iter;
+//    for(int  i = 0; i < list.Count(); i++)
+//    {
+//        string item = list[i].AsString();
+//        stream<<"List Item "<<i+1<<" : "<<item<<endl;
+//    }
+//    return stream;
+//}
+//
 }
 #endif
