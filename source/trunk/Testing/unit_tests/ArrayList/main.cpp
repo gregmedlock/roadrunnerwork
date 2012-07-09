8cdeae8f2b4ac805f402ff2964af0cc9df0f7b91
tottek <tottek@6ce1f5ab-bddf-3397-67f4-0d3c53ff36db>
2012-07-09 19:11:04 +0000

    Updated ide projects with new paths
    Changed CG environment variable RR_INSTALLS to RR_INSTALL
    Added CG environment variable RR_ROOT

--- source/trunk/Testing/unit_tests/ArrayList/main.cpp
+++ source/trunk/Testing/unit_tests/ArrayList/main.cpp
@@ -24,6 +24,7 @@ int main()
     list1.Add(list2);
     list1.Add("3");
     list1.Add("test");
+
     cout<<endl<<list1;
     cout<<endl<<"=========="<<endl;
     for(int i = 0; i < list1.Count(); i++)
@@ -32,27 +33,6 @@ int main()
         cout<<"Item "<<i<<": "<<item<<endl;
     }
 
-    ArrayList<int> l1;
-    ArrayList<int> l2;
-    ArrayList<int> l3;
-
-    l1.Add(1);
-    l1.Add(2);
-
-    l2.Add(10);
-    l2.Add(11);
-
-    l2.Add(l3);
-    l1.Add(l2);
-    l1.Add(3);
-    l1.Add(100);
-    cout<<endl<<l1;
-    cout<<endl<<"=========="<<endl;
-    for(int i = 0; i < l1.Count(); i++)
-    {
-        int item = l1[i];
-        cout<<"Item "<<i<<": "<<item<<endl;
-    }
 
     return 0;
 }
