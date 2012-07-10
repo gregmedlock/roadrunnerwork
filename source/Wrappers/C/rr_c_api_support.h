aadef9ff19ffc0eef5aa2fdb1894ee62a1e91a62
tottek <tottek@6ce1f5ab-bddf-3397-67f4-0d3c53ff36db>
2012-07-10 16:47:09 +0000

    Restructure .. again..

---
+++ source/Wrappers/C/rr_c_api_support.h
@@ -0,0 +1,41 @@
+//---------------------------------------------------------------------------
+#ifndef rr_c_api_supportH
+#define rr_c_api_supportH
+#include <string>
+#include <vector>
+#include "libstruct/lsMatrix.h"
+#include "rrStringList.h"
+#include "rrStringListContainer.h"
+#include "rrArrayList.h"
+#include "rrUtils.h"
+#include "rr_c_types.h"
+//---------------------------------------------------------------------------
+
+using std::vector;
+using std::string;
+
+namespace rr_c_api
+{
+using rr::StringList;
+using rr::ArrayList;
+using rr::RRArrayList;
+//Error/Warning Messages
+const char ALLOCATE_API_ERROR_MSG[] = {"Please allocate a handle to the roadrunner API before calling any API function"};
+
+//Internal prototypes (not exported)
+void                setError(const string& err);
+bool                copyVector(const RRVector* source, vector<double>& dest);
+
+RRVector*           createVector(const vector<double>& vec);
+vector<double>      createVector(const RRVector* vec);
+char*               createText(const char* str);        //To be deleted by client using freeText
+char*               createText(const string& str);      //To be deleted by client using freeText
+RRMatrix*           createMatrix(const LIB_LA::DoubleMatrix& mat);
+RRStringList*       createList(const rr::StringList& aList);
+RRStringList*       createList(const rr::ArrayList& aList);
+RRStringArrayList*  createList(const rr::RRArrayList<string>& aList);
+
+
+}
+
+#endif
---
+++ source/Wrappers/C/rr_version_info.h
@@ -0,0 +1,5 @@
+#ifndef rr_version_infoH
+#define rr_version_infoH
+const char* RR_VERSION =  "2012.5.31";
+
+#endif
