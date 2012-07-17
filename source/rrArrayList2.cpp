99303f0bf79224912ea2408879f726fe006c887c
tottek <tottek@6ce1f5ab-bddf-3397-67f4-0d3c53ff36db>
2012-07-17 02:41:01 +0000

    Added unit for ArrayListItems

--- source/rrArrayList2.cpp
+++ source/rrArrayList2.cpp
@@ -9,39 +9,39 @@
 namespace rr
 {
 
-ListItemBase::~ListItemBase()
+ArrayListItemBase::~ArrayListItemBase()
 {}
 
-ArrayList2Type::ArrayList2Type()
+ArrayList2Item::ArrayList2Item()
 : mValue(NULL)
 {
     mValue = new ArrayList2;
 }
 
-ArrayList2Type::~ArrayList2Type()
+ArrayList2Item::~ArrayList2Item()
 {
     delete mValue;
 }
 
-ArrayList2Type::ArrayList2Type(const ArrayList2Type& copyMe)
+ArrayList2Item::ArrayList2Item(const ArrayList2Item& copyMe)
 {
     this->mValue = new ArrayList2;
     (*this->mValue) = (*copyMe.mValue);
 }
 
-ArrayList2Type& ArrayList2Type::operator=(const ArrayList2Type& list)
+ArrayList2Item& ArrayList2Item::operator=(const ArrayList2Item& list)
 {
     (*this->mValue) = (*list.mValue);
 
     return *this;
 }
 
-ArrayList2Type::ArrayList2Type(const ArrayList2& list)
-{
-    mValue = new ArrayList2(list);
-}
+//ArrayList2Item::ArrayList2Item(const ArrayList2& list)
+//{
+//    mValue = new ArrayList2(list);
+//}
 
-unsigned int ArrayList2Type::Count() const
+unsigned int ArrayList2Item::Count() const
 {
     return (mValue) ? mValue->Count() : 0;
 }
@@ -58,7 +58,7 @@ void ArrayList2::Clear()
     }
 }
 
-const ListItemBase& ArrayList2Type::operator[](int pos) const
+const ArrayListItemBase& ArrayList2Item::operator[](int pos) const
 {
     return (*mValue)[pos];
 }
@@ -93,22 +93,22 @@ ArrayList2::ArrayList2(const ArrayList2& copyMe)
     for(u_int i = 0; i < copyMe.Count(); i++)
     {
         //const ArrayList2Item<T>& item = copyMe[i];
-        ListItemBase* ptr = const_cast<ListItemBase*>(&copyMe[i]);
-        if(dynamic_cast<ListItem<int>*>(ptr))
+        ArrayListItemBase* ptr = const_cast<ArrayListItemBase*>(&copyMe[i]);
+        if(dynamic_cast<ArrayListItem<int>*>(ptr))
         {
-            mList[i] = new ListItem<int>(*(dynamic_cast<ListItem<int>*>(ptr)));
+            mList[i] = new ArrayListItem<int>(*(dynamic_cast<ArrayListItem<int>*>(ptr)));
         }
-        else if(dynamic_cast<ListItem<double>*>(ptr))
+        else if(dynamic_cast<ArrayListItem<double>*>(ptr))
         {
-            mList[i] = new ListItem<double>(*(dynamic_cast<ListItem<double>*>(ptr)));
+            mList[i] = new ArrayListItem<double>(*(dynamic_cast<ArrayListItem<double>*>(ptr)));
         }
-        else if(dynamic_cast<ListItem<string>*>(ptr))
+        else if(dynamic_cast<ArrayListItem<string>*>(ptr))
         {
-            mList[i] = new ListItem<string>(*(dynamic_cast<ListItem<string>*>(ptr)));
+            mList[i] = new ArrayListItem<string>(*(dynamic_cast<ArrayListItem<string>*>(ptr)));
         }
-        else if(dynamic_cast<ListItem<ArrayList2Type>*>(ptr))
+        else if(dynamic_cast<ArrayListItem<ArrayList2Item>*>(ptr))
         {
-            mList[i] = new ListItem<ArrayList2Type>(*(dynamic_cast<ListItem<ArrayList2Type>*>(ptr)));
+            mList[i] = new ArrayListItem<ArrayList2Item>(*(dynamic_cast<ArrayListItem<ArrayList2Item>*>(ptr)));
         }
         else
         {
@@ -124,22 +124,22 @@ void ArrayList2::operator=(const ArrayList2& rhs)
     mList.resize(rhs.Count());
     for(u_int i = 0; i < rhs.Count(); i++)
     {
-        ListItemBase* ptr = const_cast<ListItemBase*>(&rhs[i]);
-        if(dynamic_cast<ListItem<int>*>(ptr))
+        ArrayListItemBase* ptr = const_cast<ArrayListItemBase*>(&rhs[i]);
+        if(dynamic_cast<ArrayListItem<int>*>(ptr))
         {
-            mList[i] = new ListItem<int>(*(dynamic_cast<ListItem<int>*>(ptr)));
+            mList[i] = new ArrayListItem<int>(*(dynamic_cast<ArrayListItem<int>*>(ptr)));
         }
-        else if(dynamic_cast<ListItem<double>*>(ptr))
+        else if(dynamic_cast<ArrayListItem<double>*>(ptr))
         {
-            mList[i] = new ListItem<double>(*(dynamic_cast<ListItem<double>*>(ptr)));
+            mList[i] = new ArrayListItem<double>(*(dynamic_cast<ArrayListItem<double>*>(ptr)));
         }
-        else if(dynamic_cast<ListItem<string>*>(ptr))
+        else if(dynamic_cast<ArrayListItem<string>*>(ptr))
         {
-            mList[i] = new ListItem<string>(*(dynamic_cast<ListItem<string>*>(ptr)));
+            mList[i] = new ArrayListItem<string>(*(dynamic_cast<ArrayListItem<string>*>(ptr)));
         }
-        else if(dynamic_cast<ListItem<ArrayList2Type>*>(ptr))
+        else if(dynamic_cast<ArrayListItem<ArrayList2Item>*>(ptr))
         {
-            mList[i] = new ListItem<ArrayList2Type>(*(dynamic_cast<ListItem<ArrayList2Type>*>(ptr)));
+            mList[i] = new ArrayListItem<ArrayList2Item>(*(dynamic_cast<ArrayListItem<ArrayList2Item>*>(ptr)));
         }
         else
         {
@@ -150,83 +150,40 @@ void ArrayList2::operator=(const ArrayList2& rhs)
 
 void ArrayList2::Add(const int& item)
 {
-    ListItem<int>* ptr =  new ListItem<int>(item);
+    ArrayListItem<int>* ptr =  new ArrayListItem<int>(item);
     mList.push_back(ptr);
 }
 
 void ArrayList2::Add(const double& item)
 {
-    ListItem<double>* ptr = new ListItem<double>(item);
+    ArrayListItem<double>* ptr = new ArrayListItem<double>(item);
     mList.push_back(ptr);
 }
 
 void ArrayList2::Add(const string& item)
 {
-    ListItem<string> *ptr = new ListItem<string>(item);
+    ArrayListItem<string> *ptr = new ArrayListItem<string>(item);
     mList.push_back(ptr);
 }
 
-void ArrayList2::Add(const ListItem<ArrayList2Type>& item)
+void ArrayList2::Add(const ArrayListItem<ArrayList2Item>& item)
 {
-    ListItem<ArrayList2Type> *ptr = new ListItem<ArrayList2Type>(item);
+    ArrayListItem<ArrayList2Item> *ptr = new ArrayListItem<ArrayList2Item>(item);
     mList.push_back(ptr);
 }
 
 void ArrayList2::Add(const ArrayList2& item)
 {
-    ListItem<ArrayList2Type> *aList = new ListItem<ArrayList2Type>(item);
+    ArrayListItem<ArrayList2Item> *aList = new ArrayListItem<ArrayList2Item>(item);
     mList.push_back(aList);
 }
 
-const ListItemBase& ArrayList2::operator[](int pos) const
+const ArrayListItemBase& ArrayList2::operator[](int pos) const
 {
     return *mList[pos];
 }
 
 //================== ostreams =============
-ostream& operator<<(ostream& stream, const ListItemBase& item)
-{
-    //Have to figure out subtype of item
-    ListItemBase* ptr = const_cast<ListItemBase*>(&item);
-    if(dynamic_cast<ListItem<int>*>(ptr))
-    {
-        stream << (int) *(dynamic_cast<ListItem<int>*>(ptr));
-    }
-    else if(dynamic_cast<ListItem<double>*>(ptr))
-    {
-        stream << (double) *(dynamic_cast<ListItem<double>*>(ptr));
-    }
-    else if(dynamic_cast<ListItem<string>*>(ptr))
-    {
-        stream << "\""<<(string) *(dynamic_cast<ListItem<string>*>(ptr))<<"\"";
-    }
-    else if(dynamic_cast<ListItem<ArrayList2Type>*>(ptr))
-    {
-        stream << (ArrayList2Type) *(dynamic_cast<ListItem<ArrayList2Type>*>(ptr));
-    }
-    else
-    {
-        stream<<"Stream operator not implemented for this type";
-    }
-    return stream;
-}
-
-ostream& operator<<(ostream& stream, const ArrayList2Type& item)
-{
-    stream<<"{";
-    for(int i = 0; i < item.Count(); i++)
-    {
-        stream<<item[i];
-        if(i < item.Count() -1)
-        {
-        	stream<<",";
-        }
-    }
-
-    stream<<"}";
-    return stream;
-}
-
 ostream& operator<<(ostream& stream, const ArrayList2& list)
 {
    	stream<<"{";
--- source/rrArrayList2.h
+++ source/rrArrayList2.h
@@ -3,83 +3,18 @@
 #include <vector>
 #include <string>
 #include <list>
-#include "rrObject.h"
-#include "rrStringList.h"
 #include <iostream>
-
+#include "rrArrayListItem.h"
 using namespace std;
 namespace rr
 {
 
-class RR_DECLSPEC ListItemBase : public rrObject
-{
-    public:
-        virtual ~ListItemBase();
-};
-
-ostream& operator<<(ostream& stream, const ListItemBase& item);
-
-template <class T>
-class ListItem : public ListItemBase
-{
-    private:
-        T                           value;
-
-    public:
-                                    ListItem(const T& val);
-
-        virtual                    ~ListItem(){}
-                                    operator T(){return value;}
-        virtual const char          operator[](const int& pos) const {}     //Make sense for string types
-        ListItem<T>&                operator=(const ListItem<T>& rhs);
-};
-
-template<class T>
-ListItem<T>::ListItem(const T& val)
-:
-value(val)
-{}
-
-template<>
-const char ListItem<string>::operator[](const int& pos) const 
-{
-    return (char) value[pos];
-}
-
-template<class T>
-ListItem<T>& ListItem<T>::operator=(const ListItem<T>& rhs)
-{
-    if(this != &rhs)
-    {
-        value = rhs.value;
-    }
-
-    return *this;
-}
-
-class ArrayList2;
-class RR_DECLSPEC ArrayList2Type  : public rrObject
-{
-    public:
-        ArrayList2                         *mValue;
-                                            ArrayList2Type();
-                                            ArrayList2Type(const ArrayList2Type& copyMe);                                            
-        ArrayList2Type&                     operator=(const ArrayList2Type& list);
-        
-                                            ArrayList2Type(const ArrayList2& list); //Copy constructor from ArrayList2
-                                           ~ArrayList2Type();
-        unsigned int                        Count() const;
-        const ListItemBase&                 operator[](int pos) const;
-};
-
-
-ostream& operator<<(ostream& stream, const ArrayList2Type& item);
 
 class RR_DECLSPEC ArrayList2 : public rrObject
 {
     protected:
     public:
-        vector< ListItemBase* >		   	    mList; //List of ListItemBase items
+        vector< ArrayListItemBase* >		mList; //List of ArrayListItemBase items
 
     public:
                                         	ArrayList2();
@@ -90,20 +25,14 @@ class RR_DECLSPEC ArrayList2 : public rrObject
 		void                                Add(const int& item);
 		void                                Add(const double& item);
 		void                                Add(const string& item);
-        void							    Add(const ListItem<ArrayList2Type>& item);
         void							    Add(const ArrayList2& item);
-        const ListItemBase&                 operator[](int pos) const;
+        void							    Add(const ArrayListItem<ArrayList2Item>& item);
+        const ArrayListItemBase&            operator[](int pos) const;
         void                                operator = (const ArrayList2& rhs);
 };
 
 
 ostream& operator<<(ostream& stream, const ArrayList2& list);
-
-template<>
-ListItem<ArrayList2Type>::ListItem(const ArrayList2Type& item)
-{
-    value = item;
-}
 
 }
 #endif
