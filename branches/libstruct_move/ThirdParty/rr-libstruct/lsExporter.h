3336c07c2c1f987c7d310ec64275feab6b3f2f2a
tottek <tottek@6ce1f5ab-bddf-3397-67f4-0d3c53ff36db>
2012-09-20 01:08:49 +0000

    Some changes to builds The compiler needs to be installed for all releases, therefore moved compilers from ThirdParty to ../compilers Cleaned up NLEQ a little Various CMake changes

--- branches/libstruct_move/ThirdParty/rr-libstruct/lsExporter.h
+++ branches/libstruct_move/ThirdParty/rr-libstruct/lsExporter.h
@@ -1,13 +1,14 @@
 #ifndef lsExporterH
 #define lsExporterH
 
-#if defined(WIN32)
+#if defined( WIN32 )
 
-#if defined(STATIC_LIBSTRUCT)          //Libstruct static
+#if defined( STATIC_LIBSTRUCT )          //Libstruct static
 	#define LIB_EXTERN
+
 #else
 
-#if defined(SHARED_LIBSTRUCT)       //Export libstruct
+#if defined( SHARED_LIBSTRUCT )       //Export libstruct
     #define LIB_EXTERN __declspec(dllexport)
 #else
     #define LIB_EXTERN __declspec(dllimport)
