6cff05950f1df9c62a7c4259618d81b50b8c45ca
tottek <tottek@6ce1f5ab-bddf-3397-67f4-0d3c53ff36db>
2012-09-27 19:54:09 +0000

    Got a poco thread running in /examples/threaded_rr Added CMakeFile for poco
    Updated releases(?)

---
+++ examples/threaded_rr/threaded_rr.cpp
@@ -0,0 +1,24 @@
+#include "Poco/Thread.h"
+#include "Poco/Runnable.h"
+#include <iostream>
+
+using namespace std;
+//using namespace rr;
+
+class HelloRunnable: public Poco::Runnable
+{
+    virtual void run()
+    {
+        std::cout << "Hello, world!" << std::endl;
+    }
+};
+
+int main(int argc, char** argv)
+{
+    HelloRunnable runnable;
+    Poco::Thread thread;
+    thread.start(runnable);
+    thread.join();
+    return 0;
+}
+
