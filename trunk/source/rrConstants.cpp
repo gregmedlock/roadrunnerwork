d8e61131446cf8f5f731d2d8c7e871353f54d7c3
tottek <tottek@6ce1f5ab-bddf-3397-67f4-0d3c53ff36db>
2012-09-27 18:41:50 +0000

    Added rrCOnstants

---
+++ trunk/source/rrConstants.cpp
@@ -0,0 +1,22 @@
+#ifdef USE_PCH
+#include "rr_pch.h"
+#endif
+#pragma hdrstop
+#include "rrConstants.h"
+
+//---------------------------------------------------------------------------
+namespace rr
+{
+char*       Comma = ",";
+char*       EmptyString = "";
+char*       WinPathSeparator = "\\";
+char*       LinuxPathSeparator = "/";
+
+char 	 	tab 			= '\t';
+double    	DoubleNaN   = std::numeric_limits<double>::quiet_NaN() ;
+float     	FloatNaN    = std::numeric_limits<float>::quiet_NaN() ;
+//int       	MAX_MODULE  = 512;
+
+
+}
+
