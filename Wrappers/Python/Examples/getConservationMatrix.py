ba7d54b9c22e39d290e24255fc87dc8bb113b0eb
gmedlo@gmail.com <gmedlo@gmail.com@6ce1f5ab-bddf-3397-67f4-0d3c53ff36db>
2012-10-07 20:27:31 +0000

    removed unneeded examples

--- Wrappers/Python/Examples/getConservationMatrix.py
+++
@@ -1,6 +0,0 @@
-import rrPython
-
-modelPath = ('C:\\RoadRunner\\Models\\test_1.xml')
-rrPython.loadSBMLFromFile(modelPath)
-
-print rrPython.getConservationMatrix()
--- Wrappers/Python/Examples/simulateEx.py
+++
@@ -1,5 +0,0 @@
-import rrPython
-rrPython.loadSBMLFromFile('C:\\roadRunner\\models\\simple.xml')
-results = rrPython.simulateEx(0.0,2.0,20)
-
-print results
