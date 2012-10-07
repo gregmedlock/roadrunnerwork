b8b1f197bfdd7415e7b61d142d0176b05d66ec92
gmedlo@gmail.com <gmedlo@gmail.com@6ce1f5ab-bddf-3397-67f4-0d3c53ff36db>
2012-10-07 20:27:31 +0000

    removed unneeded examples

--- trunk/Wrappers/Python/Examples/getControlCoefficients.py
+++
@@ -1,6 +0,0 @@
-import rrPython
-
-import rrPython
-rrPython.loadSBMLFromFile('C:\\roadRunner\\models\\linearChain.xml')
-
-rrPython.getScaledFluxControlCoefficientMatrix()
--- trunk/Wrappers/Python/Examples/matplotlibSimulate.py
+++
@@ -1,6 +0,0 @@
-import rrPython
-
-rrPython.loadSBMLFromFile('C:\\roadRunner\\models\\simple.xml')
-results = rrPython.simulateEx(0.0,2.0,20)
-
-#simulate function and simulateEx needs to return numpy array
